0a3105797f21aa4256560dca5bdbd08032490a37
Lukas Mauser <l.mauser@wimadev.de>
2023-07-04 19:16:26 +0200

    fix(userpost): add input validation for json input

--- userpost.php
+++ userpost.php
@@ -6,10 +6,29 @@ if (!isset($_SESSION['username'])) {
 }
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-	$json = file_get_contents("php://input");
+	$jsonString = file_get_contents("php://input");
+    $jsonData = json_decode($jsonString);
+
+    // Check if decoding was successful and if the JSON is valid
+    if ($jsonData === null && json_last_error() !== JSON_ERROR_NONE) {
+        echo('invalid data');
+		exit;
+    }
+
+	$content = $jsonData->content;
+	$role = $jsonData->role;
+
+	$sanitizedContent = htmlspecialchars($content, ENT_QUOTES, 'UTF-8');
+	$sanitizedRole = htmlspecialchars($role, ENT_QUOTES, 'UTF-8');
+	
+	if(empty($sanitizedContent) || empty($sanitizedRole)) {
+		echo('invalid data');
+		exit;
+	}
+
+	$sanitizedJsonString = "{\"role\":\"$sanitizedRole\",\"content\":\"$sanitizedContent\"}";
+	
 	$uniqid = time() . uniqid();
-	file_put_contents("feedback/$uniqid.json", $json);
-	echo $json;
+	file_put_contents("feedback/$uniqid.json", $sanitizedJsonString);
+	echo $sanitizedJsonString;
 }
-
-
