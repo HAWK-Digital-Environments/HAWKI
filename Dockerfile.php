4216f9d5a23d56c7afaae86e91a27b713ccd9a12
Mehmet Sagir <mehmet.sagir@hm.edu>
2023-09-18 14:59:30 +0000

    add dockerfile and dependencies

---
+++ Dockerfile.php
@@ -0,0 +1,12 @@
+FROM php:8.2-fpm
+#WORKDIR /var/www/html 
+COPY ./ /var/www/html/
+
+RUN apt-get update \
+    && apt-get install -y git libzip-dev zip \
+    && docker-php-ext-install zip \
+    && cd /var/www/html \
+    && chmod +x composer_install.sh && ./composer_install.sh \ 
+    && mv composer.phar /usr/local/bin/composer \
+    && composer install \
+    && rm compose.yml composer_install.sh default.conf Dockerfile.nginx Dockerfile.php
