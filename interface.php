bb4f1199579f5dd4e15ace30da4c512ea25ee32f
Mehmet Sagir <mehmet.sagir@hm.edu>
2023-09-20 12:13:32 +0000

    fix favicon part 5

--- interface.php
+++ interface.php
@@ -8,8 +8,9 @@ if (!isset($_SESSION['username'])) {
 
 ?>
 <head>
-	<title>AI Lab</title>
-	<link rel="favicon" type="image/jpg" href="/img/favicon.jpg"/>	
+	<title>HAWKI</title>
+	<link rel="shortcut icon" type="image/x-icon" href="https://mediapool.hm.edu/media/_technik/img/_technik_1/favicon.jpg" media="screen" />
+	<link rel="icon" type="image/x-icon" href="https://mediapool.hm.edu/media/_technik/img/_technik_1/favicon.jpg" media="screen" />
  </head>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 
