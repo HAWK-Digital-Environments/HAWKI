193ff57e8eef2ffd924f64f9438849d735415ef3
Mehmet Sagir <mehmet.sagir@hm.edu>
2023-09-20 10:59:17 +0000

    fix logout oidc

--- logout_oidc.php
+++ logout_oidc.php
@@ -1,16 +1,6 @@
 <?php
-require __DIR__ . '/vendor/autoload.php';
-
-use Jumbojett\OpenIDConnectClient;
-
 session_start();
-$oidc = $_SESSION['oidc'];
-unset($_SESSION['oidc']);
-$oidc -> signOut($oidc->getIdToken(), "https://ai.lab.hm.edu/logout.php");
-
-
-
 session_destroy();
-header("Location: login.php");
+header("Location: https://sso.hm.edu/idp/profile/Logout");
 exit;
 ?>
