7692a7c061b5d455bb9e07693a3b860f3af770e6
Mehmet Sagir <mehmet.sagir@hm.edu>
2023-09-20 12:45:32 +0000

    change video address

--- login.php
+++ login.php
@@ -195,7 +195,7 @@
 	  <svg viewBox="0 0 512 512" title="play-circle">
   <path d="M256 8C119 8 8 119 8 256s111 248 248 248 248-111 248-248S393 8 256 8zm115.7 272l-176 101c-15.8 8.8-35.7-2.5-35.7-21V152c0-18.4 19.8-29.8 35.7-21l176 107c16.4 9.2 16.4 32.9 0 42z" />
 </svg>
-	  <video src="https://ai.hawk.de/hawkistart.mp4" playsinline preload muted loop autoplay></video>
+	  <video src="https://ai.lab.hm.edu//hawkistart.mp4" playsinline preload muted loop autoplay></video>
 	</div>
   
 
@@ -210,7 +210,7 @@
 <div id="videoModal" class="modal">
 		<div class="modal-content">
 			<span id="closeModal" class="close">&times;</span>
-			<video src="https://ai.hawk.de/hawkistart.mp4" controls>
+			<video src="https://ai.lab.hm.edu//hawkistart.mp4" controls>
 		</div>
 	</div>
 <!-- partial -->
