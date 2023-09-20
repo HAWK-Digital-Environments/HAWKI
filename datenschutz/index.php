af9387634cce6342d8797e5f0085d6918e771e64
Mehmet Sagir <mehmet.sagir@hm.edu>
2023-09-20 07:33:22 +0000

    remove instruction

--- datenschutz/index.php
+++ datenschutz/index.php
@@ -1,5 +1,4 @@
 <?php
-$env = parse_ini_file('../.env');
 $privacyLocation = getenv("PRIVACY_LOCATION");
 
 header("Location: $privacyLocation");
--- impressum/index.php
+++ impressum/index.php
@@ -1,5 +1,4 @@
 <?php
-$env = parse_ini_file('../.env');
 $imprintLocation = getenv("IMPRINT_LOCATION");
 
 header("Location: $imprintLocation");
