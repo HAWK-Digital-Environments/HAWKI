74d46e8a18d47f39b482e391482a06c9f3da3620
Karin Borm <k.borm@ostfalia.de>
2023-10-10 11:36:47 +0200

    bugfix path to environemt file

--- impressum/index.php
+++ impressum/index.php
@@ -1,6 +1,6 @@
 <?php
-if (file_exists(".env")){
-    $env = parse_ini_file('.env');
+if (file_exists("../.env")){
+    $env = parse_ini_file('../.env');
 }	
 $imprintLocation = isset($env) ? $env["IMPRINT_LOCATION"] : getenv("IMPRINT_LOCATION");
 
