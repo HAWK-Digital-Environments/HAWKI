56b405e0d6f9af0a35d9e6f3bcba8e0fa38597f8
Ariansdf <38947447+Ariansdf@users.noreply.github.com>
2024-05-03 12:58:29 +0200

    Shibboleth open id patch (#61)
    * Correction - ODIC Var in .env.example
    
    * Correction CSRF / Logout
    
    * Typo Correction

--- private/pages/logout.php
+++ private/pages/logout.php
@@ -1,5 +1,6 @@
 <?php
     session_start();
+
     // Unset all session variables
     $_SESSION = array();
     // If it's desired, regenerate session ID (good practice for login but optional on logout)
@@ -12,12 +13,23 @@
 
     // Finally, destroy the session.
     session_destroy();
-    // Redirect to the login page
-    if ((isset($env) ? $env["Authentication"] : getenv("Authentication")) == "OIDC") {
-        // Open ID Connect
-        header('Location: oidc_logout');
-    } else {
-        header('Location: login');
+
+
+    if (file_exists(ENV_FILE_PATH)){
+        $env = parse_ini_file(ENV_FILE_PATH);
+        if($env['Authentication'] === 'Shibboleth'){
+            $redirect_uri = $env['SHIBBOLETH_LOGOUT_URL'];
+        }
+        elseif($env['Authentication'] === 'OIDC'){
+            $redirect_uri = $env['OIDC_LOGOUT_URI'];
+        }
+        else{
+            // Redirect to the login page
+            $redirect_uri ='/login';
+        }
     }
-    exit;
+    header("Location: $redirect_uri");
+    exit();
+
+
 ?>
